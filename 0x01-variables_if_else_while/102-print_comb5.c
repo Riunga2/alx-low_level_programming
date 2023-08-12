6b1d8f8ab39019458b2227d344087876ddd44d7a
Sue <sue@example.com>
2023-08-12 02:04:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,19 +13,19 @@
  */
 int main(void)
 {
-	int num1,num2;
+	int num1, num2;
 
-	for(num1=0;num1<=98;num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for(num2=num1+1;num2<=99;num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((num1/10)+'0');
+			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
-			putchar((num2 %10)+'0');
+			putchar((num2 % 10) + '0');
 
-			if(num1==98&&num2==99)
+			if (num1 == 98 && num2 == 99)
 				continue;
 
 			putchar(',');
@@ -37,4 +37,5 @@ int main(void)
 
 	return (0);
 }
+
 
