eee13aa0483379607588c721dad96d77caaeedd2
Sue <sue@example.com>
2023-08-13 05:17:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@
 /**
  * main -Entry point
  *
- * Return : Always 0(success)
+ * Return: Always 0(success)
  */
-int main (void) 
+int main(void) 
 {
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
