8c168a86d6503baf4752073430cbb9b0faf1176a
Sue <sue@example.com>
2023-08-16 19:34:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,15 @@
  
  int main (void)
 {
-	_putchar('_');
+	_putchar('-');
 	_putchar('p');
         _putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putcahr('a');
+	_putchar('a');
 	_putchar('r');
-	_putchr('\n');
+	_putchar('\n');
 	return (0);
 }
 
